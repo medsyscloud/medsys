0c30f17441c386bbacb66f1754ec94392fccc86c
medsyscloud <64090576+medsyscloud@users.noreply.github.com>
2022-05-17 08:05:15 -0500

    ssl 03

--- teclado/botones/js/ricardo.js
+++ teclado/botones/js/ricardo.js
@@ -416,7 +416,7 @@
 					break;
 
 					case "3":
-					location= 'http://www.medsyscloud003.byethost11.com/medsysweb/login.php';
+					location= 'https://www.medsyscloud003.byethost11.com/medsysweb/login.php';
 					break;
 
 					case "4":
