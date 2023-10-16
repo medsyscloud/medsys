d03e4ff77199bf964b4f4eb2bec5e94b96b51936
medsyscloud <64090576+medsyscloud@users.noreply.github.com>
2023-10-16 12:29:54 -0500

    Add files via upload

--- teclado/botones/js/ricardo.js
+++ teclado/botones/js/ricardo.js
@@ -408,7 +408,7 @@
 				// }
 				switch ($('.number-area .numbers').text()){
 					case "1":
-					location= 'https://www.medsyscloudone.byethost22.com/medsysweb/login.php';
+					location= 'https://www.medsyscloud002.byethost8.com/medsysweb01/login.php';
 					break;
 
 					case "2":
